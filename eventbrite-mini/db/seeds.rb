369e9158f02651d54eda83076c69b7a7d73de6bf
benstew <benstewart2008@gmail.com>
2016-01-12 09:49:06 -0500

    Building seed file

--- eventbrite-mini/db/seeds.rb
+++ eventbrite-mini/db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Demo User
+Person.create(firstname: "Ben", lastname: "Stewart", email: "example1234@gmail.com")
+
+#Demo Events
+Event.create(name: "Basketball Game", date: Time.now )
+Event.create(name: "Dinner", date: Time.now + 1 )
+Event.create(name: "Personal Trainer", date: Time.now + 2)
+Event.create(name: "Movie", date: Time.now + 1.5 )
+Event.create(name: "Walk The Dog", date: Time.now + 2.5 )
+
+person = Person.first
+event = Event.first
+
+#Demo Invites
+person.invites.create(event: event)
